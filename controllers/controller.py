aafcf04554d7ff9f898ba1270c7547788430ec3b
Dorothea Hinrichs <dorothea.hinrichs@posteo.net>
2022-07-21 07:56:47 +0100

    tried to do the extensions, broke my code

--- controllers/controller.py
+++ controllers/controller.py
@@ -6,3 +6,6 @@ from models.kittens import orders
 def index():
     return render_template('index.html', title='Home', orders=orders)
 
+@app.route("/orders/<id>")
+def get_order_by_id(id):
+    return render_template('order.html', order=orders[int(id)])
